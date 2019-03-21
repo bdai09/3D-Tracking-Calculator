65a56dea3b6b00e3593d1d05e48edf71a3f78b9a
bo dai <bdai9@myseneca.ca>
2019-03-21 18:44:31 -0400

    Updates

---
+++ precache-manifest.bed656b4fa4fd679f837478eb38c5550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  },
+  {
+    "revision": "ea7fbe0a28634395da7b",
+    "url": "/Calculator/static/js/main.e18b9de5.chunk.js"
+  },
+  {
+    "revision": "4dda6d3676aa7294e619",
+    "url": "/Calculator/static/js/2.4f140dab.chunk.js"
+  },
+  {
+    "revision": "ea7fbe0a28634395da7b",
+    "url": "/Calculator/static/css/main.55f29ddf.chunk.css"
+  },
+  {
+    "revision": "4c24ff9e7b64388d006659504f9ec998",
+    "url": "/Calculator/index.html"
+  }
+];
