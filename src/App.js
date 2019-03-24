38b835eca1894fade808eadd9a790c429ac3e279
bo dai <bdai9@myseneca.ca>
2019-03-23 21:26:31 -0400

    color set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
- 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -8,15 +8,40 @@ class App extends Component {
       valNew: '0',
       valOld: '0',
       formula: '',
-      beforeIs:''
+      beforeIs:'',
+      offsetX:'',
+      offsetY:'',
+      friction:1/32
     }
+    this._mouseMove=this._mouseMove.bind(this);
     this.reachMax = this.reachMax.bind(this);
     this.handleOperators = this.handleOperators.bind(this);
     this.handleEvaluate = this.handleEvaluate.bind(this);
     this.cleanUp = this.cleanUp.bind(this);
     this.handleDecimal = this.handleDecimal.bind(this);
     this.handleNumbers = this.handleNumbers.bind(this);
+    this.changeSize=this.changeSize.bind(this);
   }
+  componentDidMount() {
+    document.addEventListener('mousemove', this._mouseMove);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousemove', this._mouseMove);
+  }
+  _mouseMove(e) {
+    let followX = (window.innerWidth / 2 - e.clientX);
+    let followY = (window.innerHeight / 2 - e.clientY);
+
+    let x = 0,
+        y = 0;
+    x +=( (-followX - x) * this.state.friction);
+    y += (followY - y) * this.state.friction;
+    this.setState({
+      offsetX: x,
+      offsetY: y
+    });
+  }
+
   cleanUp() {
     this.setState({
       valNew: '0',
@@ -128,6 +153,7 @@ class App extends Component {
         formula: expression.replace(/\*/g, '⋅').replace(/-/g, '‑') + '=' + answer,
         beforeIs: ''
       });
+     // document.getElementById("formulaScreen").style.fontSize = "30px";
     }
   }
     
@@ -167,33 +193,57 @@ class App extends Component {
         }
       }
   }}
+  
+changeSize(){  //deal with re-size when formula is too long, resize font
+  if(this.state.formula!==''){
+let formulaScreen=document.getElementById('formulaScreen');
+formulaScreen.style.fontSize = "30px"
+let fontSize = parseInt(formulaScreen.style.fontSize.replace('px', ''));
+for (let i = fontSize; i >= 0; i--) {
+  let overflow=(formulaScreen.scrollHeight > formulaScreen.clientHeight || formulaScreen.scrollWidth > formulaScreen.clientWidth);
+  if (overflow) {
+   fontSize--;
+   formulaScreen.style.fontSize = fontSize + "px";
+  }
+}
+}}
 
   render() {
+    let offset = {
+      transform: `translate(-50%, -50%) perspective(600px)
+                  rotateY(${this.state.offsetX}deg)
+                  rotateX(${this.state.offsetY}deg)`
+                 }
+     this.changeSize();
     return (
-      <div className="App">
-        <div className='App-body'>
-          <div className="formulaScreen">{this.state.formula} </div>
+      <div className='wrapper' style={offset}>
+      <div className="shape"></div>
+        <div className='shape2'>
+          <div id="formulaScreen" className="formulaScreen">                  
+          {this.state.formula} </div>
           <div className="outputScreen"> {this.state.valNew} </div>
           <div>
-        <button value='AC' onClick={this.cleanUp} style={{width:160, background: '#F55A3B'}}>AC</button>
-        <button value='/'  onClick={this.handleOperators} style={{background: '#F49753'}}>/</button>
-        <button value='x'  onClick={this.handleOperators} style={{background: '#F49753'}}>x</button>
+        <button value='AC' onClick={this.cleanUp} style={{width:160, background: '#cccdce'}}>AC</button>
+        <button value='/'  onClick={this.handleOperators} style={{background: '#cccdce'}}>/</button>
+        <button value='x'  onClick={this.handleOperators} style={{background: '#cccdce'}}>x</button>
         <button value='7'  onClick={this.handleNumbers} >7</button>
         <button value='8'  onClick={this.handleNumbers} >8</button>
         <button value='9'  onClick={this.handleNumbers} >9</button>
-        <button value='‑'  onClick={this.handleOperators} style={{background: '#F49753'}}>-</button>
+        <button value='‑'  onClick={this.handleOperators} style={{background: '#cccdce'}}>-</button>
         <button value='4'  onClick={this.handleNumbers} >4</button>
         <button value='5'  onClick={this.handleNumbers} >5</button>
         <button value='6'  onClick={this.handleNumbers} >6</button>
-        <button value='+'  onClick={this.handleOperators} style={{background: '#F49753'}}>+</button>
+        <button value='+'  onClick={this.handleOperators} style={{background: '#cccdce'}}>+</button>
         <button value='1'  onClick={this.handleNumbers} >1</button>
         <button value='2'  onClick={this.handleNumbers} >2</button>
         <button value='3'  onClick={this.handleNumbers} >3</button>
-        <button value='='  onClick={this.handleEvaluate} style={{background: '#8E42C3',position: 'absolute',height: 130, bottom: 5}}>=</button>
+        <button value='='  onClick={this.handleEvaluate} style={{background: '#cccdce',position: 'absolute',height: 130, bottom: 0}}>=</button>
         <button value='0'  onClick={this.handleNumbers} style={{width:160}}>0</button>
-        <button value='.'  onClick={this.handleDecimal} >.</button>     
+        <button value='.'  onClick={this.handleDecimal} style={{width:78}}>.</button>     
+      
+      
       </div>
-        </div>
+        </div><p>Move your mouse around</p>
       </div>
     )
   }
